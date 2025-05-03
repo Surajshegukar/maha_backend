a3a17637c95674765200081f51421e7dbd6fa9c5
Surajshegukar <surajshegukar2732@gmail.com>
2025-05-03 10:23:12 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -201,7 +201,7 @@ app.get('/', (req, res) => {
   }
 );
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
